ca9e28638c60f2efe7c30381eaf2b9bb65880b1d
cdnielson <cdnielson@folkprophet.com>
2016-05-09 07:15:45 -0600

    started cart

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,13 +2,18 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:bullwhips/model/item.dart';
 
 @Component(selector: 'my-app', templateUrl: 'app_component.html')
 class AppComponent {
   String page = "home";
   String get pathToLogo => "images/title.png";
+  List<Item> cart = [];
 
   handleTopMenu(page) {
     this.page = page;
+  }
+  addToCart(item) {
+    cart.add(new Item(item, 0));
   }
 }
---
+++ lib/model/item.dart
@@ -0,0 +1,6 @@
+class Item {
+  String name;
+  int amount;
+
+  Item (this.name, this.amount);
+}
