6b3b7eb82421e7b4b4791f43c6430ae48da2d064
chani <chani@extendhealth.com>
2016-06-06 09:21:25 -0600

    a few minor updates

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -2,7 +2,7 @@ import 'package:angular2/angular2.dart';
 
 @Injectable()
 class Settings {
-  String fontStyle = "serif";
+  String fontStyle = "blkchcry";
   String fontColor = "black";
   String menuFontColor = "white";
   String fontSize = "medium";
